087f14074281fe908d344d30025244b56e20d9f7
Gautham S Shetty <gauthamsshetty@admins-MBP.lolvisitor.com>
2015-09-11 11:56:23 -0500

    Initial Commit

---
+++ TestSuperscript/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestSuperscript
+//
+//  Created by Gautham S Shetty on 9/11/15.
+//  Copyright (c) 2015 Gautham S Shetty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestSuperscript/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestSuperscript
+//
+//  Created by Gautham S Shetty on 9/11/15.
+//  Copyright (c) 2015 Gautham S Shetty. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
